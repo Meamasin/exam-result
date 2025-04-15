aaa528ebe5b80e02b7e8223dc0737be3d75e12a4
Meamasin <47712652+Meamasin@users.noreply.github.com>
2025-04-16 00:40:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let results = [];
+
+fetch('results.json')
+  .then(res => res.json())
+  .then(data => {
+    results = data;
+
+    if (window.location.pathname.includes("result.html")) {
+      const urlParams = new URLSearchParams(window.location.search);
+      const regNo = urlParams.get('reg');
+      showResult(regNo);
+    }
+  });
+
+function searchResult() {
+  const regNo = document.getElementById('regInput').value.trim();
+  const student = results.find(s => s.reg_no === regNo);
+  if (student) {
+    window.location.href = `result.html?reg=${regNo}`;
+  } else {
+    document.getElementById('error').classList.remove('hidden');
+  }
+}
+
+function showResult(regNo) {
+  const student = results.find(s => s.reg_no === regNo);
+  if (!student) return;
+
+  document.getElementById('resultCard').classList.remove('hidden');
+  document.getElementById('rno').innerText = student.reg_no;
+  document.getElementById('name').innerText = student.name;
+  document.getElementById('mark').innerText = student.mark;
+  document.getElementById('rank').innerText = student.rank;
+  document.getElementById('scholarshipText').innerText =
+    `Congratulations Dear ${student.name.toUpperCase()}, You will Get a Scholarship of ₹${student.amount}/-`;
+}
