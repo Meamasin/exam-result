2950430487aef6b2e9dbaa9ee0cb230465272e39
Nisam <47712652+Meamasin@users.noreply.github.com>
2025-04-16 14:02:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,5 @@ function showResult(regNo) {
   document.getElementById('mark').innerText = student.mark;
   document.getElementById('rank').innerText = student.rank;
   document.getElementById('scholarshipText').innerText =
-    `<blink> Congratulations Dear ${student.name.toUpperCase()}, You will Get a Scholarship of ₹${student.amount}/- </blink>`;
-   
+    `Congratulations Dear ${student.name.toUpperCase()}, You will Get a Scholarship of ₹${student.amount}/- `; 
 }
